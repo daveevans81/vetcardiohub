ebf6a180fa8a2304f875a24fc251b3db9c2e64a7
daveevans81 <dave.evans@cantab.net>
2026-02-27 22:40:42 +0000

    update nav

--- js/article-nav.js
+++ js/article-nav.js
@@ -11,37 +11,27 @@
         current += ".html";
     }
 
-    const index = chronologicalPosts.findIndex(p => p.slug === current);
-    const navContainer = document.getElementById("article-navigation");
+ const index = chronologicalPosts.findIndex(p => p.slug === current);
+const navContainer = document.getElementById("article-navigation");
 
-    if (navContainer && index !== -1) {
-        let html = `<div class="article-navigation-wrapper">`;
-
-        // Previous Link
-        if (index > 0) {
-            html += `
+if (navContainer && index !== -1) {
+    let html = `
+        <div class="nav-zone left">
+            ${index > 0 ? `
                 <a href="/blog-posts/${chronologicalPosts[index-1].slug}" class="nav-button">
                     ← ${chronologicalPosts[index-1].title}
-                </a>`;
-        } else {
-            // If there's no previous post, show "Back to Blog" on the left
-            html += `<a href="../blog.html" class="nav-button">← Back to Blog</a>`;
-        }
+                </a>` : ''}
+        </div>
 
-        // Optional: Always show a "Back to List" in the middle for consistency
-        if (index > 0) {
-            html += `<a href="../blog.html" class="nav-button">All Articles</a>`;
-        }
+        <a href="../blog.html" class="nav-link-center">All Articles</a>
 
-        // Next Link
-        if (index < chronologicalPosts.length - 1) {
-            html += `
+        <div class="nav-zone right">
+            ${index < chronologicalPosts.length - 1 ? `
                 <a href="/blog-posts/${chronologicalPosts[index+1].slug}" class="nav-button">
                     ${chronologicalPosts[index+1].title} →
-                </a>`;
-        }
-
-        html += `</div>`;
-        navContainer.innerHTML = html;
-    }
+                </a>` : ''}
+        </div>
+    `;
+    navContainer.innerHTML = html;
+}
 })();
